89c23cd21c4719edbca566ce6b937992d2a284a8
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-03-17 11:22:33 -0400

    Complete Combination Sum

--- .idea/Backtracking/ComboSum.py
+++ .idea/Backtracking/ComboSum.py
@@ -1,16 +1,21 @@
 class Solution:
+
+
+
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
         result = [];
         if (target < min(candidates)):
-            return [[]];
-        for num in candidates:
+            return [];
+        for index in range(len(candidates)):
+            num = candidates[index];
             if target == num:
-                return [[num]];
-            lst = self.combinationSum(candidates, target-num)
+                result.append([num]);
+            lst = self.combinationSum(candidates[index:], target-num)
             for innerLst in lst:
-                innerLstCopy = innerLst.copy();
-                innerLstCopy.append(num);
-                result.append(innerLstCopy);
+                if len(innerLst) > 0:
+                    innerLstCopy = innerLst.copy();
+                    innerLstCopy.insert(0, num);
+                    result.append(innerLstCopy);
         return result;
 
 sol = Solution();
