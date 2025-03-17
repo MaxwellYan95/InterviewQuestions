6e91467d158d44f75362c445e7a4f6a5ec6c0a1b
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-03-17 12:26:21 -0400

    Changed rob() to reverseRob()

--- .idea/1DDP/HouseRobber.py
+++ .idea/1DDP/HouseRobber.py
@@ -16,16 +16,16 @@ class Solution:
         elif len(nums) == 3:
             return max(nums[0]+nums[2], nums[1]);
         elif (nums[0]+nums[3] >= nums[1]+nums[3]) and (nums[0]+nums[3] >= nums[0]+nums[2]):
-            oneWay = nums[0] + nums[3] + self.rob(nums[5:]);
-            twoWay = nums[0] + nums[2] + self.rob(nums[4:]);
+            oneWay = nums[0] + nums[3] + self.reverseRob(nums[5:]);
+            twoWay = nums[0] + nums[2] + self.reverseRob(nums[4:]);
             return max(oneWay, twoWay);
         elif nums[0]+nums[2] == nums[1]+nums[3]:
-            oneWay = nums[0] + nums[2] + self.rob(nums[4:]);
-            twoWay = nums[1] + nums[3] + self.rob(nums[5:]);
+            oneWay = nums[0] + nums[2] + self.reverseRob(nums[4:]);
+            twoWay = nums[1] + nums[3] + self.reverseRob(nums[5:]);
             return max(oneWay, twoWay);
         elif nums[0]+nums[2] > nums[1]+nums[3]:
-            return nums[0] + nums[2] + self.rob(nums[4:]);
-        return nums[1] + nums[3] + self.rob(nums[5:]);
+            return nums[0] + nums[2] + self.reverseRob(nums[4:]);
+        return nums[1] + nums[3] + self.reverseRob(nums[5:]);
 
 sol = Solution();
 sol.rob([183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]);
