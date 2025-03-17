7a5151c9fbddbb59d2634b06bca6ab630c1920eb
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-03-17 15:57:17 -0400

    Still working on WorkBreak.py

--- .idea/1DDP/WordBreak.py
+++ .idea/1DDP/WordBreak.py
@@ -1,14 +1,16 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
-        sortedWord = sorted(wordDict, key=len);
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        sortedWord = sorted(wordDict, key=len, reverse=True);
+        return self.iteration(s, sortedWord);
+    def iteration(self, s: str, wordDict: List[str]) -> bool:
         if len(s) == 0:
             return True;
-        if len(sortedWord[0]) > len(s):
+        if len(wordDict[len(wordDict)-1]) > len(s):
             return False;
-        for word in sortedWord:
+        for word in wordDict:
             isLength = (len(word) <= len(s));
             if s[0: len(word)] == word and isLength:
-                iter = self.wordBreak(s[len(word):], wordDict);
+                iter = self.iteration(s[len(word):], wordDict);
                 if iter == True:
                     return True;
         return False;
