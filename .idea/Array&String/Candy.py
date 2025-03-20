2f87b4b97b3aaa50fed65b39040ae5a8d94cefd2
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-03-20 17:42:06 -0400

    Still Working on Candy.py

--- .idea/Array&String/Candy.py
+++ .idea/Array&String/Candy.py
@@ -2,26 +2,22 @@ class Solution:
     def candy(self, ratings: list[int]) -> int:
         candies = [];
         for index in range(len(ratings)):
+            candies[index] = 1;
+        for index in range(len(ratings)):
             left = -1;
-            right = -1;
             if index-1 > -1:
                 left = ratings[index-1];
-            if index+1 < len(ratings):
-                right = ratings[index+1];
             number = 1;
             curRating = ratings[index];
-            haveValues = (right != -1 and left != -1);
-            if curRating > left and right == -1:
+            haveValues = (left != -1);
+            if left > curRating and haveValues:
+                candies[index-1] = candies[index-1] + 1;
+            if curRating > left and haveValues:
                 number = candies[index-1] + 1;
-            elif curRating > right and left == -1:
-                number = number + 1;
-            elif curRating >= right and curRating > left and haveValues:
-                number = candies[index-1] + 1;
-            elif curRating > right and curRating >= left and haveValues:
-                number = candies[index+1] + 1;
             candies.append(number);
         return sum(candies);
 
 sol = Solution();
-print(sol.candy([1,2,2]));
+print(sol.candy([1, 0, 2]));
+print(sol.candy([1, 3, 2, 2, 1]));
 print();
