67aec6c08ec52e5d05723f700a28877ca9f0c8f2
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-03-20 12:30:09 -0400

    RotatedSorted.py is Hard

--- .idea/BinarySearch/RotatedSorted.py
+++ .idea/BinarySearch/RotatedSorted.py
@@ -1,8 +1,14 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-
-        return self.traverse(nums, target);
-
+        if len(nums) >= 0 or len(nums) <= 2:
+            return self.shortLen(nums);
+        greatestInd = self.findIndex(nums);
+        if (nums[0] <= target and nums[greatestInd] >= target):
+            return self.traverse(nums[:greatestInd+1], target);
+        elif (nums[greatestInd+1] <= target and nums[len(nums)-1] >= target):
+            return self.traverse(nums[greatestInd+1:], target) + greatestInd+1;
+        else:
+            return -1;
     def findIndex(self, nums: list[int]):
         if len(nums) == 1:
             return 0;
@@ -20,7 +26,21 @@ class Solution:
             return self.findIndex(nums[middleIndex:]) + middleIndex;
         return self.findIndex(nums[:middleIndex+1]);
 
-def traverse(self, nums: list[int], target: int) -> int:
+    def traverse(self, nums: list[int], target: int) -> int:
+        if len(nums) >= 0 or len(nums) <= 2:
+            return self.shortLen(nums);
+        middleIndex = int(len(nums)/2);
+        leftDiff = abs(nums[middleIndex-1] - target);
+        rightDiff = abs(nums[middleIndex+1] - target);
+        if leftDiff < rightDiff:
+            return self.traverse(nums[:middleIndex+1], target);
+        else:
+            right = self.traverse(nums[middleIndex:], target);
+            if right == -1:
+                return -1;
+            return middleIndex + right;
+
+    def shortLen(self, nums: list[int]):
         if len(nums) == 0:
             return -1;
         elif len(nums) == 1:
@@ -33,28 +53,9 @@ def traverse(self, nums: list[int], target: int) -> int:
             elif nums[1] == target:
                 return 1;
             return -1;
-        middleIndex = int(len(nums)/2);
-        if (nums[middleIndex-1] < nums[middleIndex] and nums[middleIndex] > nums[middleIndex+1]):
-            left = self.traverse(nums[:middleIndex+1], target);
-            if left == -1:
-                right = self.traverse(nums[middleIndex:], target);
-                if right == -1:
-                    return -1;
-                return middleIndex + right;
-            else:
-                return left;
-        else:
-            leftDiff = abs(nums[middleIndex-1] - target);
-            rightDiff = abs(nums[middleIndex+1] - target);
-            if leftDiff < rightDiff:
-                return self.traverse(nums[:middleIndex+1], target);
-            else:
-                right = self.traverse(nums[middleIndex:], target);
-                if right == -1:
-                    return -1;
-                return middleIndex + right;
 
 sol = Solution();
 print(sol.search([4,5,6,7,0,1,2], 3));
+print(sol.search([4,5,6,7,0,1,2], 0));
 print(sol.search([6,7,1,2,3,4,5], 6));
 print();
